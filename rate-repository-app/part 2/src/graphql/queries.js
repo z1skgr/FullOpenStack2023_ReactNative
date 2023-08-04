664ed0cbeda49a0622e8471056c427ad47aa5823
C.Ziskas <z1skgr@yahoo.gr>
2023-08-04 18:09:26 +0300

    refactor

---
+++ rate-repository-app/part 2/src/graphql/queries.js
@@ -0,0 +1,24 @@
+import { gql } from "apollo-boost";
+
+export const GET_REPOSITORIES = gql`
+  query repositories {
+    repositories {
+      edges {
+        node {
+          createdAt
+          description
+          forksCount
+          fullName
+          id
+          language
+          name
+          ownerAvatarUrl
+          ownerName
+          ratingAverage
+          reviewCount
+          stargazersCount
+        }
+      }
+    }
+  }
+`;
--- rate-repository-app/part 3/App.js
+++ rate-repository-app/part 3/App.js
@@ -1,17 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter } from 'react-router-native';
 
-import Main from './src/components/Main';
-
-
+import Main from './components/Main';
 
 const App = () => {
   return (
     <>
       <NativeRouter>
-
         <Main />
-
       </NativeRouter>
       <StatusBar style="auto" />
     </>
